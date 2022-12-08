c5b52ff409b4269b796f4f46dd8dcb212cb09254
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2022-12-08 10:23:47 +0500

    Update

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import { LeftButton, RightButton } from "chakra-ui-carousel";
+
 function Slider() {
   return (
-    <div>
-        <Box>
-      <Provider>
-        <Carousel gap={50}>
-        // Carousel content
-        </Carousel>
-        <LeftButton
-          bgColor="red.500"
-          customIcon={<ArrowLeftIcon />}
-          textColor={"white.500"}
-        />
-        <RightButton bgColor="blue.500" customIcon={<ArrowRightIcon />} />
-      </Provider>
-    </Box>
+    <div className='w-full h-[300px] border-2 border-black'>
+        <img className='w-full h-full' src="https://rukminim1.flixcart.com/fk-p-flap/844/140/image/57c85d83b66b73d0.jpg?q=50" alt="" />
     </div>
   )
 }
